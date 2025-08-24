8fbdccb7f20ca446c68db25a6649f8b72cf28171
faroutnerd <farhan91179@gmail.com>
2025-08-24 12:54:33 +0530

    message problem solved

--- client/src/features/messages/messagesSlice.js
+++ client/src/features/messages/messagesSlice.js
@@ -1,39 +1,84 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
-import api from '../../api/axios'
+// import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+// import api from '../../api/axios'
+
+// const initialState = {
+//   messages: []
+// }
+
+// export const fetchMessages = createAsyncThunk('messages/fetchMessages', async ({token, userId}) => {
+//   const {data} = await api.post('/api/message/get', {to_user_id: userId}, {
+//     headers: {Authorization: `Bearer ${token}`}
+//   });
+//   return data.success ? data : null;
+// })
+
+// const messagesSlice = createSlice({
+//   name: 'messages',
+//   initialState,
+//   reducers: {
+//     setMessages: (state, action) => {
+//       state.messages = action.payload;
+//     },
+//     addMessage: (state, action) => {
+//       state.messages = [...state.messages, action.payload];
+//     },
+//     resetMessages: (state) => {
+//       state.messages = [];
+//     },
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchMessages.fulfilled, (state, action) => {
+//         state.messages = action.payload.messages;
+//       })
+//   }
+// })
+
+// export const { setMessages, addMessage, resetMessages } = messagesSlice.actions;
+
+// export default messagesSlice.reducer
+
+
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import api from "../../api/axios";
 
 const initialState = {
-  messages: []
-}
+    messages: [],
+};
 
-export const fetchMessages = createAsyncThunk('messages/fetchMessages', async ({token, userId}) => {
-  const {data} = await api.post('/api/message/get', {to_user_id: userId}, {
-    headers: {Authorization: `Bearer ${token}`}
-  });
-  return data.success ? data : null;
-})
+export const fetchMessages = createAsyncThunk(
+    "messages/fetchMessages",
+    async ({ token, userId }) => {
+        const { data } = await api.post(
+            "/api/message/get",
+            { to_user_id: userId },
+            {
+                headers: { Authorization: `Bearer ${token}` },
+            }
+        );
+        return data.success ? data : null;
+    }
+);
 
 const messagesSlice = createSlice({
-  name: 'messages',
-  initialState,
-  reducers: {
-    setMessages: (state, action) => {
-      state.messages = action.payload;
+    name: "messages",
+    initialState,
+    reducers: {
+        setMessages: (state, action) => {
+            state.messages = action.payload;
+        },
+        addMessage: (state, action) => {
+            state.messages = [...state.messages, action.payload];
+        },
+        resetMessages: (state) => {
+            state.messages = [];
+        },
     },
-    addMessage: (state, action) => {
-      state.messages = [...state.messages, action.payload];
-    },
-    resetMessages: (state) => {
-      state.messages = [];
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchMessages.fulfilled, (state, action) => {
-        state.messages = action.payload.messages;
-      })
-  }
-})
+    extraReducers: (builder) =>
+        builder.addCase(fetchMessages.fulfilled, (state, action) => {
+            state.messages = action.payload.messages;
+        }),
+});
 
-export const { setMessages, addMessage, resetMessages } = messagesSlice.actions;
-
-export default messagesSlice.reducer
+export const { addMessage, setMessages, resetMessages } = messagesSlice.actions;
+export default messagesSlice.reducer;
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,54 +1,127 @@
-import React from 'react'
-import { dummyConnectionsData } from '../assets/assets'
-import { EyeIcon, MessageSquare } from 'lucide-react'
-import {useNavigate} from 'react-router-dom'
-import { useSelector } from 'react-redux'
+// import React from 'react'
+// import { dummyConnectionsData } from '../assets/assets'
+// import { EyeIcon, MessageSquare } from 'lucide-react'
+// import {useNavigate} from 'react-router-dom'
+// import { useSelector } from 'react-redux'
+
+// const Messages = () => {
+
+//   const {connections} = useSelector((state)=>state.connections);
+
+//   const navigate = useNavigate();
+
+//   return (
+//     <div className='min-h-screen relative bg-slate-50'>
+//       <div className='max-w-6xl mx-auto p-6'>
+//         {/* Title */}
+//         <div className='mb-8'>
+//           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
+//           <p className='text-slate-600'>Talk to your friend & family</p>
+//         </div>
+
+//         {/* Connected Users */}
+//         <div className='flex flex-col gap-3'>
+//           {connections.map((user) => (
+//             <div key={user._id} className='max-w-xl flex flex-wrap gap-5 p-6 bg-white shadow rounded-md'>
+//               <img src={user.profile_picture} alt="" className='rounded-full size-12 mx-auto' />
+//               <div className='flex-1'>
+//                 <p className='font-medium text-slate-700'>{user.full_name}</p>
+//                 <p className='text-slate-500'>@{user.username}</p>
+//                 <p className='text-sm text-gray-600'>{user.bio}</p>
+//               </div>
+
+//               <div className='flex flex-col gap-2 mt-4'> 
+//                 <button onClick={()=>navigate(`/messages/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
+//                   <MessageSquare className='w-4 h-4' /> 
+//                 </button>
+
+//                 <button onClick={()=>navigate(`/profile/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
+//                   <EyeIcon className='w-4 h-4' /> 
+//                 </button>
+
+//               </div>
+
+//             </div>
+//           ))
+//           }
+//         </div>
+
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Messages
+
+
+
+import { Eye, MessageSquare } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Messages = () => {
+    const navigate = useNavigate();
+    const { connections } = useSelector((state) => state.connections);
 
-  const {connections} = useSelector((state)=>state.connections);
-
-  const navigate = useNavigate();
-
-  return (
-    <div className='min-h-screen relative bg-slate-50'>
-      <div className='max-w-6xl mx-auto p-6'>
-        {/* Title */}
-        <div className='mb-8'>
-          <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
-          <p className='text-slate-600'>Talk to your friend & family</p>
-        </div>
-
-        {/* Connected Users */}
-        <div className='flex flex-col gap-3'>
-          {connections.map((user) => (
-            <div key={user._id} className='max-w-xl flex flex-wrap gap-5 p-6 bg-white shadow rounded-md'>
-              <img src={user.profile_picture} alt="" className='rounded-full size-12 mx-auto' />
-              <div className='flex-1'>
-                <p className='font-medium text-slate-700'>{user.full_name}</p>
-                <p className='text-slate-500'>@{user.username}</p>
-                <p className='text-sm text-gray-600'>{user.bio}</p>
-              </div>
-
-              <div className='flex flex-col gap-2 mt-4'> 
-                <button onClick={()=>navigate(`/messages/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
-                  <MessageSquare className='w-4 h-4' /> 
-                </button>
-
-                <button onClick={()=>navigate(`/profile/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
-                  <EyeIcon className='w-4 h-4' /> 
-                </button>
-
-              </div>
-
+    return (
+        <section className="relative min-h-screen bg-slate-50">
+            <div className="p-6 mx-auto max-w-6xl">
+                {/* Title */}
+                <div className="mb-8">
+                    <h1 className="text-3xl font-bold mb-2 text-slate-900">
+                        Messages
+                    </h1>
+                    <p className="text-slate-600">
+                        Talk to your friends and family
+                    </p>
+                </div>
+                {/* Connected users */}
+                <div className="flex flex-col gap-3">
+                    {connections.map((user) => (
+                        <div
+                            key={user._id}
+                            className="flex max-w-xl flex-wrap gap-5 p-6 bg-white shadow rounded-md"
+                        >
+                            <img
+                                src={user.profile_picture}
+                                alt="profile photo"
+                                className="rounded-full size-12 mx-auto"
+                            />
+                            <div className="flex-1">
+                                <p className="font-medium text-slate-700">
+                                    {user.full_name}
+                                </p>
+                                <p className="text-slate-500">
+                                    @{user.username}
+                                </p>
+                                <p className="text-sm text-gray-600">
+                                    {user.bio}
+                                </p>
+                            </div>
+                            <div className="flex flex-col gap-2 mt-4">
+                                <button
+                                    className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1 duration-200"
+                                    onClick={() =>
+                                        navigate(`/messages/${user._id}`)
+                                    }
+                                >
+                                    <MessageSquare className="size-4" />
+                                </button>
+                                <button
+                                    className="size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer duration-200"
+                                    onClick={() =>
+                                        navigate(`/profile/${user._id}`)
+                                    }
+                                >
+                                    <Eye className="size-4" />
+                                </button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-          ))
-          }
-        </div>
+        </section>
+    );
+};
 
-      </div>
-    </div>
-  )
-}
-
-export default Messages
+export default Messages;
